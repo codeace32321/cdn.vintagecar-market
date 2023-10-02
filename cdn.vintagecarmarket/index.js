8324633f4f3973f4602d8b050f460fe6178707a0
codeace32321 <jomarneri16@gmail.com>
2023-10-03 03:00:44 +0800

    rev

--- cdn.vintagecarmarket/index.js
+++ cdn.vintagecarmarket/index.js
@@ -21,48 +21,54 @@ document.addEventListener("DOMContentLoaded", function () {
             // Append the dynamically created file input to the container
             uploadInput.appendChild(fileInput);
             
-            fileInput.addEventListener('change', function () {
-                const file = fileInput.files[0];
-                const previewId = 'preview-' + inputName;
-                const previewImage = document.getElementById(previewId);
+            if (inputName === 'fileToUpload') {
+                // Handle special behavior for the thumbnail input
+                const previewImage = document.getElementById('preview-img');
                 
-                if (file) {
-                    const reader = new FileReader();
+                fileInput.addEventListener('change', function () {
+                    const file = fileInput.files[0];
                     
-                    reader.onload = function (e) {
-                        previewImage.src = e.target.result;
-                    };
-                    
-                    reader.readAsDataURL(file);
-                } else {
-                    previewImage.src = ''; // Clear the preview if no file is selected
-                }
-            });
-        });
-    });
-
-    // Add a submit event listener to the form
-    forms.forEach((form) => {
-        form.addEventListener('submit', function (e) {
-            const uploadInputs = form.querySelectorAll('[ms-code-file-upload-input]');
-            let shouldSubmit = true;
-            
-            uploadInputs.forEach((uploadInput) => {
-                const fileInput = uploadInput.querySelector('input[type="file"]');
+                    if (file) {
+                        const reader = new FileReader();
+                        
+                        reader.onload = function (e) {
+                            previewImage.src = e.target.result;
+                        };
+                        
+                        reader.readAsDataURL(file);
+                    } else {
+                        previewImage.src = ''; // Clear the preview if no file is selected
+                    }
+                });
+            } else {
+                // Handle behavior for other inputs (non-thumbnail)
+                const previewImage = document.createElement('img');
+                previewImage.setAttribute('id', 'preview-' + inputName);
                 
-                if (!fileInput || !fileInput.files[0]) {
-                    shouldSubmit = false;
-                }
-            });
-            
-            if (!shouldSubmit) {
-                e.preventDefault(); // Prevent form submission
-                alert('Please upload files for all inputs.'); // You can display a message to the user.
+                // Append the dynamically created image element for preview
+                uploadInput.appendChild(previewImage);
+                
+                fileInput.addEventListener('change', function () {
+                    const file = fileInput.files[0];
+                    const previewId = 'preview-' + inputName;
+                    const previewImage = document.getElementById(previewId);
+                    
+                    if (file) {
+                        const reader = new FileReader();
+                        
+                        reader.onload = function (e) {
+                            previewImage.src = e.target.result;
+                        };
+                        
+                        reader.readAsDataURL(file);
+                    } else {
+                        previewImage.src = ''; // Clear the preview if no file is selected
+                    }
+                });
             }
         });
     });
 });
-
 
 
 
